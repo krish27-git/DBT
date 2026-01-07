cd6377f2e885957e19e5333cf771a447b7c0f6a4
krish g <krishna.ginka27@zohomail.in>
2026-01-07 05:13:23 +0000

    D

--- models/mart/ODS/inc_model.sql
+++ models/mart/ODS/inc_model.sql
@@ -6,12 +6,10 @@
     )
 }}
 
-with cte as
-(
-    select * from KITTUDB.RAW.EMPLOYEE
+select * from KITTUDB.RAW.EMPLOYEE
 {% if is_incremental() %}
   where UPDATED_AT >= (select coalesce(max(UPDATED_AT), '1900-01-01') from {{ this }})
 {% endif %}
-)
-select * from cte
+
+
 
